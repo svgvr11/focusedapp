ebaf4f3c266ff8a1f8cdb2184972defad16be3a1
svgvr11 <victor_mfernandez@outlook.com>
2019-12-11 02:50:14 -0300

    Termino Splash Screen

---
+++ app/src/main/java/cl/endbox/focused/SplashActivity.java
@@ -0,0 +1,30 @@
+package cl.endbox.focused;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+
+    private final int DURACION_SPLASH = 1000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_activity);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
