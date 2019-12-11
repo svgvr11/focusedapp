4f990d8f3529c6b6c3ece481e99bc5f100eeae9b
svgvr11 <victor_mfernandez@outlook.com>
2019-12-11 20:30:30 -0300

    Implemento Navigation Drawer, falta editarlo

--- app/src/main/java/cl/endbox/focused/MainActivity.java
+++ app/src/main/java/cl/endbox/focused/MainActivity.java
@@ -3,13 +3,28 @@ package cl.endbox.focused;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    //Declaro Variables
+    Button btn_registro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_registro = (Button) findViewById(R.id.btnRegistro);
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MenuDrawer.class));
+            }
+        });
     }
 }
