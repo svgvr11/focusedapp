a4f968a64ffa7e8d0555c6ca2d25436bd09ff34e
svgvr11 <victor_mfernandez@outlook.com>
2019-12-12 16:58:00 -0300

    Comienzo a editar Fragment "Mis reservas"

--- app/src/main/java/cl/endbox/focused/MainActivity.java
+++ app/src/main/java/cl/endbox/focused/MainActivity.java
@@ -7,12 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Declaro Variables
     Button btn_registro;
     Button  btn_ingresar;
+    Button btn_registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
